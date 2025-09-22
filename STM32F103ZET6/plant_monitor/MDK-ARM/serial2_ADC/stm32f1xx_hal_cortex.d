fd8b92d42cdecb7eb2e583cdb136a4ea5da1075b
longzheng268 <38132402+longzheng268@users.noreply.github.com>
2025-09-23 02:44:15 +0800

    完善STM32驱动。
    STM32F10x的项目文件在项目根目录的STM32F103ZET6\MDK-ARM\serial2_ADC.uvproj。
    同时调整目录结构，使其更加规整。

---
+++ STM32F103ZET6/plant_monitor/MDK-ARM/serial2_ADC/stm32f1xx_hal_cortex.d
@@ -0,0 +1,31 @@
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Core/Inc/stm32f1xx_hal_conf.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Include/core_cm3.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: D:\Program Files\Keil v5\ARM\ARM\ARMCC\Bin\..\include\stdint.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_version.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: D:\Program Files\Keil v5\ARM\ARM\ARMCC\Bin\..\include\stddef.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_sram.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_fsmc.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+.\serial2_adc\stm32f1xx_hal_cortex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
